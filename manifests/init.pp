c69b8701200bc3d0078155e8db960163f8c9058b
Phil Friderici (ephifre) <phil.friderici@ericsson.com>
2015-08-17 17:54:11 +0200

    parameterize package management
    Change-Id: I440b7b824d7279b2f20df70549ff7eba4a0b1cf3

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,11 +3,42 @@
 # Module to manage the EIS Log Aggregation client
 #
 class logaggregation(
-  $package_name   = [ 'EISlogging', 'EISloggingNFS', ],
-  $package_ensure = 'installed',
+  $manage_packages = true,
+  $package_name    = 'USE_DEFAULTS',
+  $package_ensure  = 'installed',
 ) {
 
-  package { $package_name:
-    ensure => $package_ensure,
+  validate_bool($manage_packages)
+
+  if $manage_packages == true {
+
+    case $::osfamily {
+      'Debian': {
+        $package_name_default = [ 'eislogging', 'eisloggingnfs', ]
+      }
+      'RedHat': {
+        $package_name_default = [ 'EISlogging', 'EISloggingNFS', ]
+      }
+      'Suse': {
+        $package_name_default = [ 'EISlogging', 'EISloggingNFS', ]
+      }
+      default: {
+        fail("logaggregation support package management on osfamilies Debian, RedHat and Suse. Please set logaggregation::manage_packages to <false> for <${::osfamily}> hosts.")
+      }
+    }
+
+    if $package_name == 'USE_DEFAULTS' {
+      $package_name_real = $package_name_default
+    } else {
+      $package_name_real = any2array($package_name)
+    }
+
+    validate_array($package_name_real)
+
+    package { $package_name_real:
+      ensure => $package_ensure,
+    }
+
   }
+
 }
